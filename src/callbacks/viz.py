757a597e862eb16d8157b9b4f3c276cac2bd9185
Liu Yang <liuyang.mage@gmail.com>
2025-03-23 00:51:12 +0800

    improve viz

--- src/callbacks/viz.py
+++ src/callbacks/viz.py
@@ -1,4 +1,5 @@
 import lightning as L
+import matplotlib.pyplot as plt
 
 from . import viz_utils as vu
 
@@ -10,12 +11,16 @@ class Viz(L.Callback):
 
         figs = [[None]]  # a list of list of images (plt.figure or PIL image)
         img = vu.merge_images(figs)  # PIL image
-        img.save("test_node.png")  # just for debugging
+        img.save("test_viz.png")  # for debugging without logging to WandB
         img = vu.fig_to_wandb(img)  # convert to wandb image
+
+        plt.close("all")
+
+        valid_key = list(pl_module.cfg.data.valid.keys())[dataloader_idx]
+        valid_name = pl_module.cfg.data.valid[valid_key].name
 
         for logger in trainer.loggers:
             try:  # noqa: SIM105
-                # TODO: replace the key with dataset name
-                logger.log_image(key=f"valid_{dataloader_idx}", images=[img], step=trainer.global_step)
+                logger.log_image(key=f"{valid_name}", images=[img], step=trainer.global_step)
             except:  # noqa: E722
                 pass
