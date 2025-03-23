26d0bd9d022916d76431151f9222c077767ff634
Kong Haoming <haomingcg@163.com>
2025-03-23 16:55:45 +0800

    replace individual dimension and size attributes with combined shape attributes in default.yaml, dummy_icon.yaml, and valid dummy_icon.yaml. Update references in dataset initialization accordingly.

--- src/data/datasets/dummy_icon.py
+++ src/data/datasets/dummy_icon.py
@@ -16,40 +16,19 @@ class IconDataset(Dataset):
     # name is used for logging
     name: str = "dummy_icon_dataset"
 
-    def __init__(
-        self,
-        demo_num: int,
-        demo_cond_dim: int,
-        demo_qoi_dim: int,
-        quest_cond_dim: int,
-        quest_qoi_dim: int,
-        demo_cond_size: tuple[int, int],
-        demo_qoi_size: tuple[int, int],
-        quest_cond_size: tuple[int, int],
-        quest_qoi_size: tuple[int, int],
-    ):
+    def __init__(self, demo_num: int, cond_shape: tuple[int, int, int], qoi_shape: tuple[int, int, int]):
         self.demo_num = demo_num
-        self.demo_cond_dim = demo_cond_dim
-        self.demo_qoi_dim = demo_qoi_dim
-        self.quest_cond_dim = quest_cond_dim
-        self.quest_qoi_dim = quest_qoi_dim
-        self.demo_cond_size = demo_cond_size
-        self.demo_qoi_size = demo_qoi_size
-        self.quest_cond_size = quest_cond_size
-        self.quest_qoi_size = quest_qoi_size
+        self.cond_shape = cond_shape  # (cond_dim, cond_h, cond_w)
+        self.qoi_shape = qoi_shape  # (qoi_dim, qoi_h, qoi_w)
 
     def __len__(self):
         return 10000
 
     def __getitem__(self, idx):
-        demo_cond_features = torch.randn(
-            1, self.demo_num, self.demo_cond_dim, self.demo_cond_size[0], self.demo_cond_size[1]
-        )
-        demo_qoi_features = torch.randn(
-            1, self.demo_num, self.demo_qoi_dim, self.demo_qoi_size[0], self.demo_qoi_size[1]
-        )
-        quest_cond_features = torch.randn(1, 1, self.quest_cond_dim, self.quest_cond_size[0], self.quest_cond_size[1])
-        quest_qoi_features = torch.randn(1, 1, self.quest_qoi_dim, self.quest_qoi_size[0], self.quest_qoi_size[1])
+        demo_cond_features = torch.randn(1, self.demo_num, self.cond_shape[0], self.cond_shape[1], self.cond_shape[2])
+        demo_qoi_features = torch.randn(1, self.demo_num, self.qoi_shape[0], self.qoi_shape[1], self.qoi_shape[2])
+        quest_cond_features = torch.randn(1, 1, self.cond_shape[0], self.cond_shape[1], self.cond_shape[2])
+        quest_qoi_features = torch.randn(1, 1, self.qoi_shape[0], self.qoi_shape[1], self.qoi_shape[2])
 
         cond_features = torch.cat(
             (demo_cond_features, quest_cond_features), dim=1
