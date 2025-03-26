f8be4f5dffd4bd49b8de9ee7f8770afa7005ccfe
Kong Haoming <haomingcg@163.com>
2025-03-26 10:27:32 +0800

    not sure if the change is complete

--- src/data/datasets/dummy_icon.py
+++ src/data/datasets/dummy_icon.py
@@ -1,15 +1,7 @@
-from dataclasses import dataclass
-
 import torch
 from torch.utils.data import Dataset
 
-import src.data.data_utils as du
-
-
-@dataclass
-class IconData(du.BaseData):
-    cond_features: torch.Tensor = None
-    qoi_features: torch.Tensor = None
+from src.data.data_utils import IconData
 
 
 class IconDataset(Dataset):
@@ -30,13 +22,16 @@ class IconDataset(Dataset):
         quest_cond_features = torch.randn(1, 1, self.cond_shape[0], self.cond_shape[1], self.cond_shape[2])
         quest_qoi_features = torch.randn(1, 1, self.qoi_shape[0], self.qoi_shape[1], self.qoi_shape[2])
 
+        dummy_label = torch.zeros_like(quest_qoi_features)
         cond_features = torch.cat(
             (demo_cond_features, quest_cond_features), dim=1
         )  # (1, demo_num + 1, cond_dim, cond_h, cond_w)
-        qoi_features = torch.cat(
-            (demo_qoi_features, quest_qoi_features), dim=1
-        )  # (1, demo_num + 1, qoi_dim, qoi_h, qoi_w)
-        return IconData(
+        qoi_features = torch.cat((demo_qoi_features, dummy_label), dim=1)  # (1, demo_num + 1, qoi_dim, qoi_h, qoi_w)
+        data = IconData(
             cond_features=cond_features,
             qoi_features=qoi_features,
         )
+
+        label = quest_qoi_features
+
+        return {"data": data, "label": label}
