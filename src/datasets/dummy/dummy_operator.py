0ec4d7169754daccb29405a1896ce99e536e707b
Liu Yang <liuyang.mage@gmail.com>
2025-04-09 00:40:35 +0800

    slight cleanup of datasets

--- src/datasets/dummy/dummy_operator.py
+++ src/datasets/dummy/dummy_operator.py
@@ -5,9 +5,6 @@ from src.datasets.data_utils import BaseLabelData, OperatorData
 
 
 class DummyOperatorDataset(Dataset):
-    # name is used for logging
-    name: str = "dummy_operator_dataset"
-
     def __init__(self, f_seq_len: int, g_seq_len: int, f_inout_dim: int, g_in_dim: int, g_out_dim: int):
         self.f_seq_len = f_seq_len
         self.g_seq_len = g_seq_len
@@ -16,7 +13,7 @@ class DummyOperatorDataset(Dataset):
         self.g_out_dim = g_out_dim
 
     def __len__(self):
-        return 10000
+        return 1000
 
     def __getitem__(self, idx):
         f_samples = torch.randn(1, self.f_seq_len, self.f_inout_dim)
@@ -24,10 +21,10 @@ class DummyOperatorDataset(Dataset):
         g_targets = torch.randn(1, self.g_seq_len, self.g_out_dim)
 
         data = OperatorData(
-            description=[f"data from {self.name}, idx: {idx}, random state: {torch.randn(1).item()}"],
+            description=[f"data from {self.__class__.__name__}, idx: {idx}, random state: {torch.randn(1).item()}"],
             f_samples=f_samples,
             g_inputs=g_inputs,
         )
-        label = BaseLabelData(description=[f"label from {self.name}"], label=g_targets)
+        label = BaseLabelData(description=[f"label from {self.__class__.__name__}"], label=g_targets)
 
         return {"data": data, "label": label}
--- src/datasets/dummy/dummy_vicon.py
+++ src/datasets/dummy/dummy_vicon.py
@@ -5,16 +5,13 @@ from src.datasets.data_utils import BaseLabelData, ViconData
 
 
 class DummyViconDataset(Dataset):
-    # name is used for logging
-    name: str = "dummy_vicon_dataset"
-
     def __init__(self, demo_num: int, cond_shape: tuple[int, int, int], qoi_shape: tuple[int, int, int]):
         self.demo_num = demo_num
         self.cond_shape = cond_shape  # (cond_dim, cond_h, cond_w)
         self.qoi_shape = qoi_shape  # (qoi_dim, qoi_h, qoi_w)
 
     def __len__(self):
-        return 10000
+        return 1000
 
     def __getitem__(self, idx):
         demo_cond = torch.randn(1, self.demo_num, self.cond_shape[0], self.cond_shape[1], self.cond_shape[2])
@@ -23,12 +20,12 @@ class DummyViconDataset(Dataset):
         quest_qoi = torch.randn(1, 1, self.qoi_shape[0], self.qoi_shape[1], self.qoi_shape[2])
 
         data = ViconData(
-            description=[f"data from {self.name}, idx: {idx}, random state: {torch.randn(1).item()}"],
+            description=[f"data from {self.__class__.__name__}, idx: {idx}, random state: {torch.randn(1).item()}"],
             demo_cond=demo_cond,
             demo_qoi=demo_qoi,
             quest_cond=quest_cond,
         )
 
-        label = BaseLabelData(description=[f"label from {self.name}"], label=quest_qoi)
+        label = BaseLabelData(description=[f"label from {self.__class__.__name__}"], label=quest_qoi)
 
         return {"data": data, "label": label}
