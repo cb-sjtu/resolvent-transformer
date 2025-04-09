58be02fc26055b91578dbebd84e49a9e8bb8dac0
Liu Yang <liuyang.mage@gmail.com>
2025-04-09 12:00:24 +0800

    update rng in dataloader

--- src/datamodules/base_datamodule.py
+++ src/datamodules/base_datamodule.py
@@ -1,4 +1,8 @@
+import random
+from functools import partial
+
 import hydra
+import numpy as np
 import torch
 from lightning import LightningDataModule
 from omegaconf import DictConfig
@@ -8,22 +12,46 @@ import src.datasets.data_utils as du
 from src.datamodules.dataloader import CycleLoader
 
 
-def get_worker_init_fn(rank: int, seed: int = None):
+def custom_worker_seed_fn(worker_id: int, rank: int, base_seed: int | None, dataset_name: str, print_seed: bool):
     """
-    Returns a function that initializes the worker with a random seed.
-    This is important to ensure that each worker gets a different seed
-    and therefore different data.
+    custom seed for each worker
+    seeds ALWAYS vary across different workers on the same device
+    case 0: base_seed is None, rank = 0
+            seeds vary across different calls of this function, but are shared across different devices
+            essentially doing nothing, i.e. worker_init_fn = None (except for numpy and random seeding and printing)
+            this is usually for training, but we want to keep the same seed across different devices
+            so that the (random) reshape can be synchronized across different devices (for acceleration)
+    case 1: base_seed is None, rank = global_rank
+            seeds vary across different calls of this function, and vary across different devices
+            this is usually for training with full randomness
+    case 3: base_seed is not None, rank = 0
+            seeds are fixed across different calls of this function and different devices
+            this is not common
+    case 4: base_seed is not None, rank = global_rank
+            seeds are fixed across different calls of this function, but vary across different devices
+            this is usually for validation/testing, so different epochs can be fairly compared
+            validation/testing dataset should be deterministic in general, but some cases require RNG within the dataset
+            if RNG is used, validation/testing results will not be reproducible if batches or the num_workers is changed
     """
-    if seed is None:  # if no seed is provided, use a random seed
-        seed = torch.randint(0, 0xFFFF_FFFF, (1,)).item()
-    seed = seed % 0xFFFF_FFFF  # make sure the seed is in the range of 0 to 0xffff_ffff
+    if print_seed:
+        print(
+            f"dataset: {dataset_name}, rank: {rank}, worker_id: {worker_id}, "
+            f"original initial_seed: {torch.initial_seed()}",
+            flush=True,
+        )
 
-    def worker_init_fn(worker_id):
-        # set the random seed for each worker
-        # max seed: 0xffff_ffff_ffff_ffff
-        torch.manual_seed((rank * 0xFFFF + worker_id) * 0xFFFF_FFFF + seed)
-
-    return worker_init_fn
+    # worker_seed should include the base_seed and the worker_id
+    worker_seed = torch.initial_seed() if base_seed is None else (base_seed + worker_id)
+    seed = (rank * 1000 + worker_seed) % 0xFFFF_FFFF_FFFF_FFFF
+    torch.manual_seed(seed)
+    np.random.seed(seed % 0xFFFF_FFFF)
+    random.seed(seed % 0xFFFF_FFFF)
+    if print_seed:
+        print(
+            f"dataset: {dataset_name}, rank: {rank}, worker_id: {worker_id}, "
+            f"updated initial_seed: {torch.initial_seed()}",
+            flush=True,
+        )
 
 
 def collate_fn(raw_list: list[dict]):
@@ -42,7 +70,7 @@ class BaseDataModule(LightningDataModule):
         self.save_hyperparameters(logger=False)
         self.cfg = cfg
 
-        self.need_distributed = "ddp" in cfg.trainer.strategy
+        self.need_distributed = cfg.trainer.get("strategy") is not None and "ddp" in cfg.trainer.strategy
 
     def prepare_data(self) -> None:
         """Download data if needed. Lightning ensures that `self.prepare_data()` is called only
@@ -75,10 +103,9 @@ class BaseDataModule(LightningDataModule):
         # TODO: move dataset initialization here
         pass
 
-    def dataloader_from_cfg(self, cfg, skip_distributed: bool = False):
+    def train_dataloader_from_cfg(self, cfg):
         """
-        geometry will be mixed in one batch.
-        return: datalooper.
+        return a DataLoader for training
         """
         # use instantiate to get the dataset, since different config may use different Dataset class
         dataset = hydra.utils.instantiate(cfg.dataset)
@@ -90,26 +117,65 @@ class BaseDataModule(LightningDataModule):
             "collate_fn": collate_fn,
         }
 
-        if self.need_distributed and not skip_distributed:
-            rank = self.trainer.global_rank
-            worker_init_fn = get_worker_init_fn(rank) if cfg.random_across_devices else None
+        # if random_across_devices: seeds vary across different devices
+        # it's safe to use global_rank even if not distributed
+        # if not random_across_devices: seeds are shared across different devices (with worker-specific variations)
+        # essentially doing nothing
+        worker_init_fn = partial(
+            custom_worker_seed_fn,
+            rank=self.trainer.global_rank if cfg.random_across_devices else 0,
+            base_seed=None,
+            dataset_name=cfg.name,
+            print_seed=self.cfg.print_lv >= 2,
+        )
+
+        # if not distributed, a plain DataLoader is enough
+        if not self.need_distributed:
             return DataLoader(
-                dataset=dataset,
-                sampler=DistributedSampler(
-                    dataset=dataset,
-                    shuffle=True,
-                    drop_last=True,
-                ),
-                worker_init_fn=worker_init_fn,
-                **common_kwargs,
+                dataset=dataset, shuffle=True, drop_last=True, worker_init_fn=worker_init_fn, **common_kwargs
             )
-        else:
-            return DataLoader(
-                dataset=dataset,
-                shuffle=True,
-                drop_last=True,
-                **common_kwargs,
-            )
+
+        # if distributed, use DistributedSampler
+        # we will wrap the dataloader in CycleLoader,
+        # therefore lightning cannot automatically handle DistributedSampler
+        return DataLoader(
+            dataset=dataset,
+            sampler=DistributedSampler(dataset=dataset, shuffle=True, drop_last=True),
+            worker_init_fn=worker_init_fn,
+            **common_kwargs,
+        )
+
+    def valid_test_dataloader_from_cfg(self, cfg):
+        """
+        return a DataLoader for validation or test
+        """
+        # use instantiate to get the dataset, since different config may use different Dataset class
+        dataset = hydra.utils.instantiate(cfg.dataset)
+
+        common_kwargs = {
+            "batch_size": cfg.batch_size_per_device,
+            "num_workers": cfg.num_workers,
+            "pin_memory": cfg.pin_memory,
+            "collate_fn": collate_fn,
+        }
+
+        # always use global_rank
+        worker_init_fn = partial(
+            custom_worker_seed_fn,
+            rank=self.trainer.global_rank,
+            base_seed=cfg.base_seed,
+            dataset_name=cfg.name,
+            print_seed=self.cfg.print_lv >= 2,
+        )
+
+        # Since we're not using CycleLoader here, we can rely on Lightning's built-in handling of DistributedSampler
+        return DataLoader(
+            dataset=dataset,
+            shuffle=False,
+            drop_last=False,  # careful: different from train_dataloader
+            worker_init_fn=worker_init_fn,
+            **common_kwargs,
+        )
 
     def train_dataloader(self):
         """
@@ -117,18 +183,17 @@ class BaseDataModule(LightningDataModule):
         """
         dataloaders = []
         for _i, (_key, cfg) in enumerate(self.cfg.data.train.items()):
-            dataloaders.append(self.dataloader_from_cfg(cfg))
+            dataloaders.append(self.train_dataloader_from_cfg(cfg))
         return CycleLoader(dataloaders)
 
     def val_dataloader(self):
         """
-        return a list of dataloopers for separate validation
+        return a list of dataloaders for separate validation
         """
         dataloaders = []
         for _i, (_key, cfg) in enumerate(self.cfg.data.valid.items()):
-            dataloaders.append(self.dataloader_from_cfg(cfg, skip_distributed=True))
-            #! Note that we skip distributed sampler for validation since `L.Trainer` will take care if it by default
-        return dataloaders
+            dataloaders.append(self.valid_test_dataloader_from_cfg(cfg))
+        return dataloaders  # don't wrap with CycleLoader
 
     def test_dataloader(self):
         pass
